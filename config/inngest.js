c003194a27fa2a55f7e9c0990973a21e0152d7e2
Styx <qwertyqaz81315@gmail.com>
2025-05-20 22:53:33 +0800

    fix clerk

--- config/inngest.js
+++ config/inngest.js
@@ -11,7 +11,7 @@ export const syncUserCreation = inngest.createFunction(
         id: "sync-user-from-clerk",
     },
     {
-        event: 'clear/user.created',
+        event: 'clerk/user.created'
     },
     async ({event }) =>{
         const { id, first_name, last_name, email_addresses, image_url} = event.data
